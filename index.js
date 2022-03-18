b4ef343afad808ae0158bc3acecd7ec0daec6487
Michael Davis <jrmdd1496@gmail.com>
2022-03-18 10:50:00 -0500

    commit tree f8cd8b699c97b4f994903d7e696370136b607f08

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+let cats = ['Milo','Otis','Garfield'];
+
+function destructivelyAppendCat() {
+    cats.splice(3,0,"Ralph");
+    return cats;
+}
+
+function destructivelyPrependCat() {
+    cats.splice(0,0, "Bob");
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+
+function appendCat(name) {
+    return [...cats,name];
+}
+
+function prependCat(name) {
+    return [name,...cats];
+}
+
+function removeFirstCat() {
+    let newCats = [...cats.slice(1,3)];
+    return newCats;
+}
+
+function removeLastCat() {
+    let newCats = [...cats.slice(0,2)];
+    return newCats;
+}
